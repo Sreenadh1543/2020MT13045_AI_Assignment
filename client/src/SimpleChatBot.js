a9800b8755b1d2ca63ade56484ddd5c3a254e0e1
Sreenadh Mahavadi <sreenadh1543@gmail.com>
2023-11-16 23:40:55 +0530

    Added a chat bot to application

--- client/src/SimpleChatBot.js
+++ client/src/SimpleChatBot.js
@@ -1,9 +1,21 @@
 import React , {Component} from "react";
+import Chatbot from 'react-chatbot-kit';
+import 'react-chatbot-kit/build/main.css';
+import config from './Config.js';
+import ActionProvider from './ActionProvider.js';
+import MessageParser from './MessageParser.js';
+
 
 function SimpleChatBot(){
     return (
        <div>
          <h1>Simple Chat bot with Random Responses!</h1>
+         <div className="chatbot">
+             <Chatbot
+              config={config}
+              messageParser={MessageParser}
+              actionProvider={ActionProvider}/>
+        </div>
        </div>
     );
 
